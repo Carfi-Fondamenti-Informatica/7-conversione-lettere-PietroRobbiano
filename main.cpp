11ad0479005335de096e6766ecbedc052253152c
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-08 19:34:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include<iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main (){
+    char carattere[1];
+    for ( int i=0; i<1 ; i++){
+        cin >> carattere[i];
+    }
+    if ( controllo(carattere[0]) == 1 ){
+        cout << conversione(carattere[0])<< endl;
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
+
