f8d33e0e21fddbea2134e9c22ebb8fcb067ba1e9
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-08 19:35:18 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+bool controllo( char a ){
+    if ( ((a >= 65)&&(a <=90))||(a >=97)&&( a <=122) ){
+        return true;
+    } else {
+        return false;
+    }
+}
 
+char conversione( char a ) {
+        if ((a >= 65)&&(a <=90)){
+            char convertito[1];
+            convertito[0] = a +32;
+            return convertito[0];
+        } if ( (a >=97)&&( a <=122) ){
+            char convertito[1];
+            convertito[0] = a -32;
+            return convertito[0];
+        }
+}
